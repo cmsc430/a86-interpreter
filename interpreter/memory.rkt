d30f55e425bcec4ff06ff6c2c1ec6ffe6ac85824
Pierce Darragh <pierce.darragh@gmail.com>
2022-06-01 11:54:15 -0600

    interpreter/memory: update comments

--- interpreter/memory.rkt
+++ interpreter/memory.rkt
@@ -16,6 +16,13 @@
 ;; The size of words, given in bytes.
 (define word-size-bytes (/ word-size-bits 8))
 
+;; Given either an integer or a byte string, returns a string representing that
+;; value in either binary or hexadecimal.
+;;
+;;   mode:
+;;       determines how to format the value; accepted formats are
+;;           'binary OR 'bin OR 'b      - binary formatting
+;;           'hexadecimal OR 'hex OR 'h - hexadecimal formatting
 (define (format-word value [mode 'binary])
   (match mode
     [(or 'binary 'bin 'b)
@@ -23,27 +30,46 @@
     [(or 'hexadecimal 'hex 'h)
      (~r value #:base 16 #:min-width word-size-bytes #:pad-string "0")]))
 
+;; Given an address, produces the next word-aligned address from the given
+;; address's word-aligned address.
 (define (next-word-aligned-address address)
   (- (align-address-to-word address) word-size-bytes))
 
+;; Given an address, produces the next lowest word-aligned address, according to
+;; the value of [word-size-bytes]. If the given address is word-aligned, it is
+;; returned unchanged.
 (define (align-address-to-word address)
   (- address (modulo address word-size-bytes)))
 
+;; Determines whether an address is properly word-aligned, according to the
+;; value of [word-size-bytes].
 (define (aligned-to-word? address)
   (= 0 (modulo address word-size-bytes)))
 
-;; Memory is a mapping from Addresses to vectors of Cells. Each Cell is a pair
-;; of a time tick with a value.
+;; A pair of a time tick with a value.
+;; TODO: Remove transparency?
 (struct Cell (tick value) #:transparent)
 
-;; TODO: comment
+;; A representation of runtime memory.
 ;;
-;;   max-address: the highest address available
-;;   min-address: the lowest address available
-;;   memhash: a box containing the hashmap from addresses to immutable vectors
-;;            of Cells
-;;   handling: one of the memory handling strategies
-;;   max-depth: the maximum depth if a limited strategy is used
+;;   max-address:
+;;       The highest address available.
+;;
+;;   min-address:
+;;       The lowest address available.
+;;
+;;   memhash:
+;;       A mutable hashmap from addresses to immutable vectors of Cells.
+;;
+;;   handling:
+;;       One of the memory handling strategies.
+;;
+;;   max-depth:
+;;       The maximum depth to be used if a non-unlimited strategy is employed.
+;;
+;;   error-on-overwrite:
+;;       Whether to prevent attempts by [memory-set!] to overwrite addresses
+;;       that currently hold non-byte-string values.
 (struct Memory (max-address
                 min-address
                 memhash
@@ -60,6 +86,46 @@
 ;; Defines the maximum amount of values a Cell will hold.
 (define max-cell-depth 10)
 
+;; Sets up the memory to be used during emulation. All arguments are optional.
+;;
+;;   instructions:
+;;       A list of instructions to include in the initial (highest) memory
+;;       addresses. Anything given in this list will be written without first
+;;       checking if the value is a [bytes?].
+;;
+;;       default: (list)
+;;
+;;   max-address:
+;;       The highest address available. This is the "first" address to be used,
+;;       rounded down to the nearest word boundary.
+;;
+;;       default: #xffffffffffffffff
+;;
+;;   min-address:
+;;       The lowest address available. Memory cannot be written beyond this.
+;;
+;;       default: #xffff800000000000
+;;
+;;   handling-strategy:
+;;       Memory can be set up to only allow a specific number of writes to a
+;;       given address in memory. This is to help with performance, since old
+;;       (overwritten) values can be kept around to help with debugging. The
+;;       possible options are:
+;;           'unlimited - there is no bound on the number of writes
+;;           'limited   - only [max-depth] writes are allowed, after which an
+;;                        error will be raised
+;;           'rotating  - the address's history is treated as a rotating buffer
+;;                        with only [max-depth] entries, meaning once
+;;                        [max-depth] writes are performed, new writes will
+;;                        "push out" the oldest entries
+;;
+;;       default: 'unlimited
+;;
+;;   max-depth:
+;;       The maximum amount of writes to allow, depending on the
+;;       [handling-strategy] being used.
+;;
+;;       default: 10
 (define (initialize-memory [instructions (list)]
                            [max-address #xffffffffffffffff]
                            [min-address #xffff800000000000]
@@ -81,7 +147,7 @@
             max-depth
             error-on-initialized-overwrite)))
 
-;; An empty set of bytes.
+;; Produces an empty set of bytes.
 (define (make-empty-bytes) (bytes->immutable-bytes (make-bytes word-size-bytes 0)))
 
 ;; Retrieves the value stored at the indicated address. If no value is present
@@ -111,6 +177,10 @@
          (>= (memory-depth memory address)
              (Memory-max-depth memory)))))
 
+;; Determines whether the indicated address holds a special (non-byte-string)
+;; value. Since instructions are written into memory during memory
+;; initialization, this is a way to check if the address is holding an
+;; instruction.
 (define (specialized-initial-value? memory address)
   (not (bytes? (memory-ref memory address))))
 
@@ -141,13 +211,7 @@
      (raise-user-error 'memory-set!
                        "cannot use time ticks with values less than 0; got ~a" tick)]
     [else
-     (hash-set! (Memory-memhash memory)
-                address
-                (apply vector-immutable
-                       (cons (Cell tick value)
-                             (vector->list (hash-ref (Memory-memhash memory)
-                                                     address
-                                                     (vector-immutable))))))]))
+     (unsafe-memory-set! memory address tick value)]))
 
 ;; The same as [memory-set!], but does not perform any safety checks.
 (define (unsafe-memory-set! memory address tick value)
