0ee047c109c943ba633be98c3ffc82d9a03c4402
Pierce Darragh <pierce.darragh@gmail.com>
2022-06-02 18:41:27 -0600

    interpreter/state: extend State fields and begin 'step' implementation

--- interpreter/state.rkt
+++ interpreter/state.rkt
@@ -4,27 +4,96 @@
          "memory.rkt"
          "registers.rkt")
 
-(provide (all-defined-out))
+(provide initialize-state
+         step)
 
-;; TODO: comment
-(struct State (labels registers flags memory) #:transparent)
+;; A representation of the current state of the machine. The state includes:
+;;
+;;   time-tick:
+;;       An integer representing how many instructions have been evaluated.
+;;
+;;   program-counter:
+;;       The address of the next instruction to execute.
+;;
+;;   labels:
+;;       An association list mapping label names to addresses.
+;;
+;;   registers:
+;;       A hash mapping register names to their values.
+;;
+;;   flags:
+;;       A hash mapping flag names to their values.
+;;
+;;   memory:
+;;       A [Memory] object from ["memory.rkt"], representing the current stack.
+;;
+;; TODO: Remove transparency?
+;; TODO: Revise implementations of recording devices for consistency? Currently,
+;; the registers and flags are meant to be updated functionally while the
+;; memory is stateful.
+(struct State (time-tick program-counter labels registers flags memory) #:transparent)
 
-;; TODO: comment
+;; Given a [Program], initializes the machine state.
 (define (initialize-state program)
   (let*-values
-      ([(sp memory) (initialize-memory (Program-instructions program))]
+      ([(pc sp memory)
+        (initialize-memory (Program-instructions program))]
        [(labels)
         (memory-fold (λ (labels addr value)
                        (if (Label? value)
-                           (cons (cons addr (Label-x value)) labels)
+                           (cons (cons (Label-x value) addr) labels)
                            labels))
                      (list)
                      memory
                      #f sp)]
        [(registers)
         (hash-set new-registers 'rsp sp)])
-    (State labels registers new-flags memory)))
+    (State 0 pc labels registers new-flags memory)))
 
-;; TODO: Implement this, then an [interp] function that calls it repeatedly.
+;; Given a machine state, takes the next step according to the a86 semantics.
 (define (step state)
-  state)
+  (match state
+    [(State tick pc labels registers flags memory)
+     (match (memory-ref memory pc)
+       [(Label _)
+        (State (add1 tick)
+               (next-word-aligned-address pc)
+               labels registers flags memory)]
+       [(Ret)
+        (error 'step-Ret)]
+       [(Call x)
+        (error 'step-Call)]
+       [(Mov dst src)
+        (error 'step-Mov)]
+       [(Add dst src)
+        (error 'step-Add)]
+       [(Sub dst src)
+        (error 'step-Sub)]
+       [(Cmp a1 a2)
+        (error 'step-Cmp)]
+       [(Jmp t)
+        (error 'step-Jmp)]
+       [(Je t)
+        (error 'step-Je)]
+       [(Jne t)
+        (error 'step-Jne)]
+       [(Jl t)
+        (error 'step-Jl)]
+       [(Jg t)
+        (error 'step-Jg)]
+       [(And dst src)
+        (error 'step-And)]
+       [(Or dst src)
+        (error 'step-Or)]
+       [(Xor dst src)
+        (error 'step-Xor)]
+       [(Sal dst i)
+        (error 'step-Sal)]
+       [(Sar dst i)
+        (error 'step-Sar)]
+       [(Push a1)
+        (error 'step-Push)]
+       [(Pop a1)
+        (error 'step-Pop)]
+       [(Lea dst x)
+        (error 'step-Lea)])]))
