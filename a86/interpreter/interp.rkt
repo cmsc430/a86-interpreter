7fe56226b5ee88df42ec1bc086eb610d66596237
Pierce Darragh <pierce.darragh@gmail.com>
2022-10-05 23:02:48 -0400

    a86/interpreter/interp: use correct function name (oops)

--- a86/interpreter/interp.rkt
+++ a86/interpreter/interp.rkt
@@ -8,8 +8,8 @@
 (define (interp instructions
                 #:entry-point [entry-point 'entry]
                 #:runtime [runtime (hash)])
-  (match (interp (initialize-state (Program instructions)
-                                   #:entry-point entry-point
-                                   #:runtime runtime))
+  (match (multi-step (initialize-state (Program instructions)
+                                       #:entry-point entry-point
+                                       #:runtime runtime))
     [(cons state _)
      (hash-ref (State-registers state) 'rax)]))
