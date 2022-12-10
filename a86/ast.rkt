b02cfa577888b89078552be8666b2cc5110ebaa9
Pierce Darragh <pierce.darragh@gmail.com>
2022-12-09 21:57:41 -0500

    a86/ast: add new instructions

--- a86/ast.rkt
+++ a86/ast.rkt
@@ -5,7 +5,9 @@
          racket/set
          (for-syntax syntax/parse))
 
-(provide (rename-out [sequence seq]))
+(provide (rename-out [sequence seq])
+         64-bit-integer?
+         32-bit-integer?)
 
 (define check:label-symbol
   (λ (x n)
@@ -29,6 +31,14 @@
     (unless (or (symbol? x) (offset? x)); either register or label
       (error n "expects symbol; given ~v" x))
     x))
+
+(define check:cmov
+  (λ (a a1 a2 n)
+    (unless (register? a1)
+      (error n "expects register; given ~v" a1))
+    (unless (or (register? a2) (offset? a2))
+      (error n "expects register or offset; given ~v" a2))
+    (values a a1 a2)))
 
 (define check:arith
   (λ (a1 a2 n)
@@ -133,13 +143,23 @@
   (Label  (x)       check:label-symbol)
   (Global (x)       check:label-symbol)
   (Extern (x)       check:label-symbol)
-  ;; Regular instructions.
+  ;; Stack maintenance.
+  (Push   (a1)      check:push)
+  (Pop    (a1)      check:register)
+  ;; Function calls.
   (Ret    ())
   (Call   (x)       check:target)
-  (Mov    (dst src) check:src-dest)
+  ;; Arithmetic operations.
+  (Not    (x)       check:register)
   (Add    (dst src) check:arith)
   (Sub    (dst src) check:arith)
+  (And    (dst src) check:src-dest)
+  (Or     (dst src) check:src-dest)
+  (Xor    (dst src) check:src-dest)
   (Cmp    (a1 a2)   check:src-dest)
+  (Sal    (dst i)   check:shift)
+  (Sar    (dst i)   check:shift)
+  ;; Jumps.
   (Jmp    (x)       check:target)
   (Je     (x)       check:target)
   (Jne    (x)       check:target)
@@ -147,13 +167,23 @@
   (Jle    (x)       check:target)
   (Jg     (x)       check:target)
   (Jge    (x)       check:target)
-  (And    (dst src) check:src-dest)
-  (Or     (dst src) check:src-dest)
-  (Xor    (dst src) check:src-dest)
-  (Sal    (dst i)   check:shift)
-  (Sar    (dst i)   check:shift)
-  (Push   (a1)      check:push)
-  (Pop    (a1)      check:register)
+  (Jo     (x)       check:target)
+  (Jno    (x)       check:target)
+  (Jc     (x)       check:target)
+  (Jnc    (x)       check:target)
+  ;; Moves.
+  (Mov    (dst src) check:src-dest)
+  (Cmove  (dst src) check:cmov)
+  (Cmovne (dst src) check:cmov)
+  (Cmovl  (dst src) check:cmov)
+  (Cmovle (dst src) check:cmov)
+  (Cmovg  (dst src) check:cmov)
+  (Cmovge (dst src) check:cmov)
+  (Cmovo  (dst src) check:cmov)
+  (Cmovno (dst src) check:cmov)
+  (Cmovc  (dst src) check:cmov)
+  (Cmovnc (dst src) check:cmov)
+  ;; Other instructions.
   (Lea    (dst x)   check:lea)
   (Div    (den)     check:register)
   (Equ    (x v)     check:label-symbol+integer)
