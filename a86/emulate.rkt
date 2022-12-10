79c8da10dcf3ba415a98e19b4d287e302d53b7fc
Pierce Darragh <pierce.darragh@gmail.com>
2022-12-09 23:43:02 -0500

    a86/emulate: correctly extract initial label

--- a86/emulate.rkt
+++ a86/emulate.rkt
@@ -14,23 +14,26 @@
 ;; Asm -> Value
 ;; Interpret (by using an emulator) x86-64 code
 ;; Assume: entry point is "entry"
-(define (asm-emulate instructions #:entry-point [entry-point 'entry])
+(define (asm-emulate instructions)
   (let-values ([(text-contents
                  data-contents
                  rodata-contents
                  bss-contents
                  globals  ;; TODO: necessary for something?
                  externs  ;; TODO: necessary for something?
-                 labels->indices)
+                 label-index-assocs)
                 (separate-instructions (sequence (Text)
-                                                 (Call entry-point)
+                                                 (Call (gensym 'entry))
                                                  instructions))])
-    (let* ([memory (make-memory #:text-contents text-contents
+    (let* ([initial-label (car (car (reverse label-index-assocs)))]
+           [text-contents (append (take text-contents (sub1 (length text-contents)))
+                                  (list (Call initial-label)))]
+           [memory (make-memory #:text-contents text-contents
                                 #:data-contents data-contents
                                 #:rodata-contents rodata-contents
                                 #:bss-contents bss-contents)]
            [instruction-pointer (cdr (section->range memory text))]
-           [labels->addresses (compute-label-addresses labels->indices
+           [labels->addresses (compute-label-addresses label-index-assocs
                                                        instruction-pointer)])
       (match (multi-step (initialize-state memory)
                          memory
@@ -41,31 +44,33 @@
 ;; Converts the indices assigned to each label into addresses in the .text
 ;; section. It is assumed that all indices will properly lie within the bounds
 ;; of the .text section.
-(define (compute-label-addresses labels->indices hi-text-address)
-  (for/hash ([(label index) (in-hash labels->indices)])
-    (values label (word-aligned-offset hi-text-address (* -1 index)))))
+(define (compute-label-addresses label-index-assocs hi-text-address)
+  (for/hash ([pair label-index-assocs])
+    (values (car pair)
+            (word-aligned-offset hi-text-address
+                                 (* -1 (cdr pair))))))
 
 ;; Separates a flat list of instructions into .text, .data, .rodata, and .bss
 ;; sections while also extracting global declarations, external declarations,
 ;; and indexed subroutine labels.
 (define (separate-instructions instructions)
-  (let separate ([instructions instructions]
-                 [index 0]
-                 [section text]
-                 [text-contents '()]
-                 [data-contents '()]
-                 [rodata-contents '()]
-                 [bss-contents '()]
-                 [globals (set)]
-                 [externs (set)]
-                 [labels->indices (hash)])
+  (let separate ([instructions       instructions]
+                 [index              0]
+                 [section            text]
+                 [text-contents      '()]
+                 [data-contents      '()]
+                 [rodata-contents    '()]
+                 [bss-contents       '()]
+                 [globals            (set)]
+                 [externs            (set)]
+                 [label-index-assocs (list)])
     (match instructions
       [(list)
        ;; NOTE: We intentionally do not reverse these lists. This way, the
        ;; contents are written into memory in a top-down manner.
        ;; TODO: Is this the right behavior?
        (values text-contents data-contents rodata-contents bss-contents
-               globals externs labels->indices)]
+               globals externs label-index-assocs)]
       [(cons instruction instructions)
        (let ([separate (λ (#:add-index [add-index   #t]  ;; This one is true!
                            #:section   [new-section #f]  ;; The rest are false.
@@ -87,7 +92,7 @@
                                    (or new-bss     bss-contents)
                                    (or new-globals globals)
                                    (or new-externs externs)
-                                   (or new-labels  labels->indices)))])
+                                   (or new-labels  label-index-assocs)))])
          (match instruction
            [(Text)     (separate #:add-index #f #:section text)]
            [(Data)     (separate #:add-index #f #:section data)]
@@ -95,7 +100,7 @@
            [(Bss)      (separate #:add-index #f #:section bss)]
            [(Global g) (separate #:add-index #f #:globals (set-add globals g))]
            [(Extern e) (separate #:add-index #f #:externs (set-add externs e))]
-           [(Label l)  (separate #:add-index #f #:labels  (hash-set labels->indices l index))]
+           [(Label l)  (separate #:add-index #f #:labels  (cons (cons l index) label-index-assocs))]
            [(or (Dd x) (Dq x))
             (match section
               ['data   (separate #:add-index #f #:data   (cons x data-contents))]
