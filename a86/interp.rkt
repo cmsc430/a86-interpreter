799bc901dc8208e1ce18bc8562d6b57601dd3119
Pierce Darragh <pierce.darragh@gmail.com>
2023-12-29 01:28:24 -0700

    Copy new interp.rkt from ziggy

--- a86/interp.rkt
+++ a86/interp.rkt
@@ -1,12 +1,28 @@
 #lang racket
+
 (provide/contract
  [current-objs  (parameter/c (listof path-string?))]
  [asm-interp    (-> (listof instruction?) any/c)]
  [asm-interp/io (-> (listof instruction?) string? any/c)])
 
-(require "printer.rkt" "ast.rkt" "callback.rkt"
+(require "callback.rkt"
+         "check-nasm.rkt"
+         "instructions.rkt"
+         "printer.rkt"
+         "utility.rkt"
+         (submod "printer.rkt" private)
+
          (rename-in ffi/unsafe [-> _->]))
-(require (submod "printer.rkt" private))
+
+(define-logger a86)
+
+;; Check NASM availability when required to fail fast.
+(check-nasm-available)
+
+(define *debug*?
+  (let ([r (getenv "PLTSTDERR")])
+    (and r
+         (string=? r "info@a86"))))
 
 ;; Assembly code is linked with object files in this parameter
 (define current-objs
@@ -29,10 +45,42 @@
 
 (define fmt (if (eq? (system-type 'os) 'macosx) 'macho64 'elf64))
 
+;; WARNING: The heap is re-used, so make sure you're done with it
+;; before calling asm-interp again
+(define *heap*
+  ; IMPROVE ME: hard-coded heap size
+  (malloc _int64 20000 'raw))
+
+;; Integer64 -> String
+(define (int64->binary-string n)
+  (format "#b~a"
+          (~r n #:base 2 #:min-width 64 #:pad-string "0")))
+
+;; Integer64 -> String
+(define (int64->octal-string n)
+  (format "#o~a"
+          (~r n #:base 8 #:min-width 22 #:pad-string "0")))
+
+;; Integer64
+(define (int64->hex-string n)
+  (format "#x~a"
+          (~r n #:base 16 #:min-width 16 #:pad-string "0")))
+
+(define (show-state . regs)
+  (format "\n~a"
+          (map (λ (r v)
+                 (format "(~a ~a)" r (int64->hex-string v)))
+               '(rax rbx rcx rdx rbp rsp rsi rdi
+                     r8 r9 r10 r11 r12 r13 r14 r15 instr flags)
+               regs)))
+
 ;; Asm String -> (cons Value String)
 ;; Like asm-interp, but uses given string for input and returns
 ;; result with string output
 (define (asm-interp/io a input)
+
+  (log-a86-info (~v a))
+
   (define t.s   (make-temporary-file "nasm~a.s"))
   (define t.o   (path-replace-extension t.s #".o"))
   (define t.so  (path-replace-extension t.s #".so"))
@@ -42,8 +90,10 @@
   (with-output-to-file t.s
     #:exists 'truncate
     (λ ()
-      (parameterize ((current-shared? #t))
-        (asm-display a))))
+      (parameterize ([current-shared? #t])
+        (asm-display (if *debug*?
+                         (debug-transform a)
+                         a)))))
 
   (nasm t.s t.o)
   (ld t.o t.so)
@@ -66,28 +116,32 @@
     (set-ffi-obj! "error_handler" libt.so _pointer
                   (function-ptr (λ () (raise 'err)) (_fun _-> _void))))
 
+  (when *debug*?
+    (define log (ffi-obj-ref log-label libt.so (thunk #f)))
+    (when log
+      (set-ffi-obj! log-label libt.so _pointer
+                    (function-ptr
+                     (λ () (log-a86-info
+                            (apply show-state
+                                   (build-list 18 (λ (i) (ptr-ref log _int64 (add1 i)))))))
+                     (_fun _-> _void)))))
 
-  (define current-heap #f)
+  (define has-heap? #f)
 
-  ;; allocate a heap
   (when (ffi-obj-ref "heap" libt.so (thunk #f))
-    (set! current-heap (make-c-parameter "heap" libt.so _pointer))
+    (set! has-heap? #t)
 
-    (if (ffi-obj-ref "from" libt.so (thunk #f))
-        (begin
-          (current-heap
-           ; IMPROVE ME: hard-coded heap size
-           (malloc _int64 20000 'raw))
-          (set-ffi-obj! "from" libt.so _pointer (current-heap))
-          (set-ffi-obj! "to" libt.so _pointer (ptr-add (current-heap) 10000 _int64))
-          (set-ffi-obj! "types" libt.so _pointer (malloc _int32 10000)))
-        (current-heap
-         ; IMPROVE ME: hard-coded heap size
-         (malloc _int64 10000 'raw))))
+    ;; This is a GC-enabled run-time so set from, to, and types space
+    (when (ffi-obj-ref "from" libt.so (thunk #f))
+      ;; FIXME: leaks types memory
+      (set-ffi-obj! "from"  libt.so _pointer *heap*)
+      (set-ffi-obj! "to"    libt.so _pointer (ptr-add *heap* 10000 _int64))
+      (set-ffi-obj! "types" libt.so _pointer (malloc _int32 10000))))
 
   (delete-file t.s)
   (delete-file t.o)
   (delete-file t.so)
+
   (if input
       (let ()
         (unless (and (ffi-obj-ref "in" libt.so (thunk #f))
@@ -106,15 +160,9 @@
         (current-out (fopen t.out "w"))
 
         (define result
-          (begin0
-            (with-handlers ((symbol? identity))
-              (guard-foreign-escape
-               (if current-heap
-                   (cons (current-heap) (entry (current-heap)))
-                   (entry #f))))
-            #;
-            (when current-heap
-              (free (current-heap)))))
+          (with-handlers ([symbol? identity])
+            (guard-foreign-escape
+             (entry *heap*))))
 
         (fflush (current-out))
         (fclose (current-in))
@@ -125,20 +173,14 @@
         (delete-file t.out)
         (cons result output))
 
-      (begin0
-        (with-handlers ((symbol? identity))
-          (guard-foreign-escape
-           (if current-heap
-               (cons (current-heap) (entry (current-heap)))
-               (entry #f))))
-        #;
-        (when current-heap
-          (free (current-heap))))))
+      (with-handlers ([symbol? identity])
+        (guard-foreign-escape
+         (entry *heap*)))))
 
 
 (define (string-splice xs)
   (apply string-append
-         (add-between (map (lambda (s) (string-append "\"" s "\"")) xs)
+         (add-between (map (λ (s) (string-append "\"" s "\"")) xs)
                       " ")))
 
 ;;; Utilities for calling nasm and linker with informative error messages
@@ -156,7 +198,7 @@
 ;; run nasm on t.s to create t.o
 (define (nasm t.s t.o)
   (define err-port (open-output-string))
-  (unless (parameterize ((current-error-port err-port))
+  (unless (parameterize ([current-error-port err-port])
             (system (format "nasm -f ~a ~a -o ~a" fmt t.s t.o)))
     (nasm:error (get-output-string err-port))))
 
@@ -179,7 +221,7 @@
     (if (eq? (system-type 'os) 'macosx)
         ""
         "-z defs "))
-  (unless (parameterize ((current-error-port err-port))
+  (unless (parameterize ([current-error-port err-port])
             (system (format "gcc ~a-v -shared ~a ~a -o ~a"
                             -z-defs-maybe
                             t.o objs t.so)))
@@ -189,3 +231,69 @@
                (regexp-match #rx"undefined reference to `(.*)'" err-msg)) ; linux
       [(list _ symbol) (ld:undef-symbol symbol)]
       [_ (ld:error (format "unknown link error.\n\n~a" err-msg))])))
+
+
+
+;; Debugging facilities
+
+(define log-label (symbol->label (gensym 'log)))
+
+(define (Log i)
+  (seq (save-registers)
+       (Pushf)
+       (Mov 'rax i)
+       (Mov (Offset log-label (* 8 17)) 'rax)
+       (Mov 'rax (Offset 'rsp 0))
+       (Mov (Offset log-label (* 8 18)) 'rax)
+       (Call (Offset log-label 0))
+       (Popf)
+       (restore-registers)))
+
+(define (instrument is)
+  (for/fold ([ls '()]
+             #:result (reverse ls))
+            ([idx (in-naturals)]
+             [ins (in-list is)])
+    (if (serious-instruction? ins)
+        (seq ins (reverse (Log idx)) ls)
+        (seq ins ls))))
+
+(define (serious-instruction? ins)
+  (match ins
+    [(Label _) #f]
+    [(Global _) #f]
+    [(? comment?) #f]
+    [_ #t]))
+
+(define (debug-transform is)
+  (seq (instrument is)
+          ;; End of user program
+          (Data)
+          (Global log-label)
+          (Label log-label)
+          (Dq 0) ; callback placeholder
+          (static-alloc-registers)
+          (Dq 0) ; index of instruction
+          (Dq 0) ; flags
+          ))
+
+(define registers
+  '(rax rbx rcx rdx rbp rsp rsi rdi
+        r8 r9 r10 r11 r12 r13 r14 r15))
+
+(define (static-alloc-registers)
+  (apply seq
+         (map (λ (r) (seq (Dq 0) (% (~a r))))
+              registers)))
+
+(define (save-registers)
+  (apply seq
+         (map (λ (r i) (seq (Mov (Offset log-label (* 8 i)) r)))
+              registers
+              (build-list (length registers) add1))))
+
+(define (restore-registers)
+  (apply seq
+         (map (λ (r i) (seq (Mov r (Offset log-label (* 8 i)))))
+              registers
+              (build-list (length registers) add1))))
