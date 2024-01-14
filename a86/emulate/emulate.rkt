0b4495af193ba59606a176998daafe2591f35663
Pierce Darragh <pierce.darragh@gmail.com>
2024-01-14 17:29:13 -0500

    Reformulate emulator encapsulation for easier parameterization
    Also adds [reload-emulator] to restart from within the evaluation loop.

--- a86/emulate/emulate.rkt
+++ a86/emulate/emulate.rkt
@@ -1,7 +1,32 @@
 #lang racket
 
 (provide current-emulator
-         emulator-result
+
+         persist-current-emulator?
+
+         current-emulator-before-thunk
+         current-emulator-body-thunk
+         current-emulator-after-thunk
+         current-emulator-exit-handler
+         current-emulator-exn?-handler
+         current-emulator-raise-handler
+
+         default-emulator-before-thunk
+         default-emulator-before-thunk/io
+         default-emulator-body-thunk
+         default-emulator-body-thunk/io
+         default-emulator-after-thunk
+         default-emulator-after-thunk/io
+         default-emulator-exit-handler
+         default-emulator-exit-handler/io
+         default-emulator-exn?-handler
+         default-emulator-exn?-handler/io
+         default-emulator-raise-handler
+         default-emulator-raise-handler/io
+
+         run-emulator
+         reload-emulator
+
          asm-emulate
          asm-emulate/io)
 
@@ -10,52 +35,227 @@
          "stacktrace.rkt"
          "../utility.rkt")
 
-(define emulator-result (make-parameter #f))
+;; Whether to persist the [current-emulator] beyond the scope of [run-emulator].
+(define persist-current-emulator? (make-parameter #f))
 
-;; Asm -> Value
-;; Interpret (by using an emulator) x86-64 code
+;; The continuation prompt currently in use by the emulator.
+;;
+;; NOTE: Do not export!
+(define current-emulator-continuation-prompt-tag
+  (make-parameter (default-continuation-prompt-tag)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Emulation Parameters
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Called immediately after emulator initialization, but before the
+;; [current-emulator-body-thunk] is called. Result discarded.
+(define current-emulator-before-thunk  (make-parameter #f))
+;; Called after [current-emulator-before-thunk]. This should handle the
+;; operation of the emulator.
+(define current-emulator-body-thunk    (make-parameter #f))
+;; Called after [current-emulator-body-thunk], but while [current-emulator] is
+;; still parameterized.
+(define current-emulator-after-thunk   (make-parameter #f))
+;; Called if [exit] is called during evaluation of the
+;; [current-emulator-body-thunk].
+(define current-emulator-exit-handler  (make-parameter #f))
+;; Called if an [exn?] is raised during evaluation of the
+;; [current-emulator-body-thunk].
+(define current-emulator-exn?-handler  (make-parameter #f))
+;; Called if a non-[exn?] is raised during evaluation of the
+;; [current-emulator-body-thunk].
+;;
+;; NOTE: This can be useful for terminating evaluation early through the
+;; runtime, e.g., to "raise exceptions" within the program being interpreted.
+(define current-emulator-raise-handler (make-parameter #f))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Default Parameter Values
+;;
+;; The names are [default-<foo>] for non-I/O emulation and [default-<foo>/io]
+;; for I/O emulation, where [current-<foo>] is the name of the corresponding
+;; parameter (defined above).
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; By default, nothing occurs before the body, but it's nice to have options!
+(define default-emulator-before-thunk    void)
+(define default-emulator-before-thunk/io void)
+
+;; By default, the emulator is run until either the program halts or it takes
+;; [emulator-step-count] steps.
+(define default-emulator-body-thunk    (λ () (emulator-multi-step!)))
+(define default-emulator-body-thunk/io (λ () (emulator-multi-step!)))
+
+;; Both of the default after-thunks retrieve the value of [rax] and decode it to
+;; a signed integer form, since that's what happens in our existing C-based
+;; runtime system.
+;;
+;; The I/O version also returns the [current-runtime-output-port], and the two
+;; values are [cons]ed. If the [current-runtime-output-port] is a
+;; [string-port?], then the string representation of its contents is returned
+;; instead.
+(define default-emulator-after-thunk
+  (λ ()
+    (let ([r (emulator-register-ref 'rax)])
+      (if (a86-value? r)
+          (a86-value->signed-integer r)
+          r))))
+(define default-emulator-after-thunk/io
+  (λ ()
+    (let ([r (emulator-register-ref 'rax)]
+          [p (current-runtime-output-port)])
+      (cons (if (a86-value? r)
+                (a86-value->signed-integer r)
+                r)
+            (if (string-port? p)
+                (get-output-string p)
+                p)))))
+
+;; If the program is exited prematurely, an exception is raised.
+(define default-emulator-exit-handler    (λ (v) (raise-user-error "program exited with status ~a" v)))
+(define default-emulator-exit-handler/io (λ (v) (raise-user-error "program exited with status ~a" v)))
+
+;; When an exception occurs during execution, the exception is reproduced and a
+;; stack trace is given.
+;;
+;; The I/O version also shows what input was not consumed yet, and what output
+;; was captured prior the exception being raised.
+(define default-emulator-exn?-handler
+  (λ (e) (raise-user-error "error encountered during evaluation:\n\n~a\nmachine stack trace:\n~a"
+                           (exn-message e)
+                           (format-stacktrace (current-emulator)))))
+(define default-emulator-exn?-handler/io
+  (λ (e) (raise-user-error "error encountered during I/O evaluation:\n\n~a\n  machine stack trace:\n~a\n  input unused: ~v\n  output captured: ~v"
+                           (exn-message e)
+                           (string-join (for/list ([str (in-list (string-split
+                                                                  (format-stacktrace (current-emulator))
+                                                                  "\n"))])
+                                          (if (non-empty-string? str)
+                                              (string-append "  " str)
+                                              str))
+                                        "\n")
+                           (string-append*
+                            (let read-loop ([strs '()])
+                              (match (read-string 8 (current-runtime-input-port))
+                                [(? eof-object?) (reverse strs)]
+                                [str (read-loop (cons str strs))])))
+                           (and (string-port? (current-runtime-output-port))
+                                (get-output-string (current-runtime-output-port))))))
+
+;; Non-exceptions that are raised are returned as the result of the emulation.
+(define default-emulator-raise-handler    (λ (e) e))
+(define default-emulator-raise-handler/io (λ (e) e))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Running the Emulator
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; The basic interface for running the interpreter. The arguments are a list of
+;; a86 instructions, an input port, and an output port. The operation of the
+;; interpreter can be customized by parameterization.
+;;
+;;   1. The [current-emulator] is initialized with the [instructions].
+;;   2. The runtime input and output ports are parameterized.
+;;   3. The [current-emulator-before-thunk] is evaluated.
+;;   4. The exit handler and exception handlers are parameterized.
+;;   5. The [current-emulator-body-thunk] is evaluated within this scope.
+;;   6. Outside of that scope but still within the scope where
+;;      [current-emulator] is parameterized, the [current-emulator-after-thunk]
+;;      is evaluated.
+;;
+;; If [persist-current-emulator?] contains a non-[#f] value, the
+;; [current-emulator] parameter will remain set to the emulator used by this
+;; interpretation after [run-emulator] has finished evaluating. This can be
+;; useful for debugging.
+;;
+;; In normal operation, the result of [run-emulator] is the result of
+;; [current-emulator-after-thunk]. If the program is [exit]ed, an [exn?] is
+;; raised, or a non-[exn?] is raised, the result of [run-emulator] is the result
+;; of [current-emulator-exit-handler], [current-emulator-exn?-handler], or
+;; [current-emulator-raise-handler], respectively.
+(define (run-emulator instructions input-port output-port)
+
+  (parameterize ([current-emulator-continuation-prompt-tag
+                  (make-continuation-prompt-tag 'run-emulator)])
+
+    (define (emulator-proc instructions input-port output-port)
+      (let ([emulator (initialize-emulator instructions)])
+        (when ((persist-current-emulator?))
+          (current-emulator emulator))
+        (parameterize ([current-emulator emulator]
+                       [current-runtime-input-port   input-port]
+                       [current-runtime-output-port output-port])
+          ((current-emulator-before-thunk))
+          (parameterize ([exit-handler (current-emulator-exit-handler)])
+            (with-handlers ([exn?     (current-emulator-exn?-handler)]
+                            [(λ _ #t) (current-emulator-raise-handler)])
+              ((current-emulator-body-thunk))))
+          ((current-emulator-after-thunk)))))
+
+    (define (emulator-prompt-handler instructions input-port output-port)
+      (call-with-continuation-prompt emulator-proc
+                                     (current-emulator-continuation-prompt-tag)
+                                     emulator-prompt-handler
+                                     instructions
+                                     input-port
+                                     output-port))
+
+    (emulator-prompt-handler instructions input-port output-port)))f
+
+;; Like [run-emulator], but only works when an emulator is already running. This
+;; is useful when you want to emulate multiple programs within the same
+;; emulation environment.
+(define (reload-emulator instructions input-port output-port)
+  (if (eq? (current-emulator-continuation-prompt-tag)
+           (default-continuation-prompt-tag))
+      (raise-user-error 'reload-emulator "emulator not active; cannot reload")
+      (abort-current-continuation (current-emulator-continuation-prompt-tag)
+                                  instructions input-port
+                                  output-port)))
+
+;; Calls [run-emulator] with the default parameters for non-I/O interpretation.
+;; However, parameters can either be overridden by using a keyword argument or
+;; by performing the parameterization in a surrounding scope.
 (define (asm-emulate instructions
-                     #:after [after-thunk (λ ()
-                                            (let ([r (emulator-result)])
-                                              (if (a86-value? r)
-                                                  (a86-value->signed-integer r)
-                                                  r)))]
-                     #:on-exit [exit-thunk (λ () (void))]
-                     #:on-raise [raise-thunk after-thunk])
-  (asm-emulate/io instructions #f #f #:after after-thunk #:on-exit exit-thunk #:on-raise raise-thunk))
+                     #:before   [  before-thunk #f]
+                     #:during   [    body-thunk #f]
+                     #:after    [   after-thunk #f]
+                     #:on-exit  [ on-exit-thunk #f]
+                     #:on-exn   [  on-exn-thunk #f]
+                     #:on-raise [on-raise-thunk #f])
+  (parameterize
+      ([current-emulator-exit-handler  (or on-exit-thunk  (current-emulator-exit-handler)  default-emulator-exit-handler)]
+       [current-emulator-exn?-handler  (or on-exn-thunk   (current-emulator-exn?-handler)  default-emulator-exn?-handler)]
+       [current-emulator-raise-handler (or on-raise-thunk (current-emulator-raise-handler) default-emulator-raise-handler)]
+       [current-emulator-before-thunk  (or before-thunk   (current-emulator-before-thunk)  default-emulator-before-thunk)]
+       [current-emulator-body-thunk    (or body-thunk     (current-emulator-body-thunk)    default-emulator-body-thunk)]
+       [current-emulator-after-thunk   (or after-thunk    (current-emulator-after-thunk)   default-emulator-after-thunk)])
+    (run-emulator instructions #f #f)))
 
+;; Calls [run-emulator] with the default parameters for I/O interpretation.
+;; However, parameters can either be overridden by using a keyword argument or
+;; by performing the parameterization in a surrounding scope.
 (define (asm-emulate/io instructions
                         input-port
                         [output-port (open-output-string)]
-                        #:after [after-thunk (λ ()
-                                               (let ([r (emulator-result)]
-                                                     [p (current-runtime-output-port)])
-                                                 (cons (if (a86-value? r)
-                                                           (a86-value->signed-integer r)
-                                                           r)
-                                                       (if (string-port? p)
-                                                           (get-output-string p)
-                                                           p))))]
-                        #:on-exit [exit-thunk (λ () (void))]
-                        #:on-raise [raise-thunk after-thunk])
-  (current-emulator (initialize-emulator instructions))
-  (parameterize ([current-runtime-input-port input-port]
-                 [current-runtime-output-port output-port]
-                 [exit-handler (λ (v)
-                                 (exit-thunk)
-                                 (raise-user-error 'asm-emulate/io
-                                                   "program exited with status ~a"
-                                                   v))])
-    (with-handlers ([exn?
-                     (λ (e)
-                       (raise-user-error
-                        (format "error encountered during evaluation:\n\n~a\nmachine stack trace:\n~a"
-                                (exn-message e)
-                                (format-stacktrace (current-emulator)))))]
-                    [(λ _ #t)
-                     (λ (e)
-                       (parameterize ([emulator-result e])
-                         (raise-thunk)))])
-      (emulator-multi-step! (current-emulator))
-      (parameterize ([emulator-result (emulator-register-ref (current-emulator) 'rax)])
-        (after-thunk)))))
+                        #:before   [  before-thunk #f]
+                        #:during   [    body-thunk #f]
+                        #:after    [   after-thunk #f]
+                        #:on-exit  [ on-exit-thunk #f]
+                        #:on-exn   [  on-exn-thunk #f]
+                        #:on-raise [on-raise-thunk #f])
+  (parameterize
+      ([current-emulator-exit-handler  (or on-exit-thunk  (current-emulator-exit-handler)  default-emulator-exit-handler/io)]
+       [current-emulator-exn?-handler  (or on-exn-thunk   (current-emulator-exn?-handler)  default-emulator-exn?-handler/io)]
+       [current-emulator-raise-handler (or on-raise-thunk (current-emulator-raise-handler) default-emulator-raise-handler/io)]
+       [current-emulator-before-thunk  (or before-thunk   (current-emulator-before-thunk)  default-emulator-before-thunk/io)]
+       [current-emulator-body-thunk    (or body-thunk     (current-emulator-body-thunk)    default-emulator-body-thunk/io)]
+       [current-emulator-after-thunk   (or after-thunk    (current-emulator-after-thunk)   default-emulator-after-thunk/io)])
+    (run-emulator instructions input-port output-port)))
--- a86/emulate/main.rkt
+++ a86/emulate/main.rkt
@@ -1,44 +1,50 @@
 #lang racket
 
 (provide (contract-out
-          [char                   etype?]
-          [uchar                  etype?]
-          [int64                  etype?]
-          [int32                  etype?]
-          [uint64                 etype?]
-          [uint32                 etype?]
-          ;; TODO: Export more runtime functionality from here? Or elsewhere?
-          [step-count             (parameter/c integer?)]
-          [current-emulator       (parameter/c emulator?)]
-          [emulator-result        (parameter/c a86-value?)]
-          [emulator-flag-ref      (case-> (-> flag? boolean?)
-                                          (-> emulator? flag? boolean?)
-                                          (-> emulator? nonnegative-integer? flag? boolean?))]
-          [emulator-register-ref  (case-> (-> register? a86-value?)
-                                          (-> emulator? register? a86-value?)
-                                          (-> emulator? nonnegative-integer? register? a86-value?))]
-          [emulator-memory-ref    (case-> (-> address? a86-value?)
-                                          (-> emulator? address? a86-value?)
-                                          (-> emulator? nonnegative-integer? address? a86-value?))]
-          [emulator-memory-ref/32 (case-> (-> address? a86-value/32-bit?)
-                                          (-> emulator? address? a86-value/32-bit?)
-                                          (-> emulator? nonnegative-integer? address? a86-value/32-bit?))]
-          [convert                (-> integer? etype? result/c)]
-          [ptr-ref                (->* [a86-value? etype?]
-                                       (nonnegative-integer?)
-                                       result/c)]
-          [asm-emulate            (->* [(listof instruction?)]
-                                       [#:after    (-> any/c)
-                                        #:on-exit  (-> any/c)
-                                        #:on-raise (-> any/c)]
-                                       any/c)]
-          [asm-emulate/io         (->* [(listof instruction?)
-                                        input-port?]
-                                       [output-port?
-                                        #:after    (-> any/c)
-                                        #:on-exit  (-> any/c)
-                                        #:on-raise (-> any/c)]
-                                       any/c)]))
+          [char                      etype?]
+          [uchar                     etype?]
+          [int64                     etype?]
+          [int32                     etype?]
+          [uint64                    etype?]
+          [uint32                    etype?]
+          [emulator-flag-ref         (case-> (->                                flag?     boolean?)
+                                             (-> emulator?                      flag?     boolean?)
+                                             (-> emulator? nonnegative-integer? flag?     boolean?))]
+          [emulator-register-ref     (case-> (->                                register? a86-value?)
+                                             (-> emulator?                      register? a86-value?)
+                                             (-> emulator? nonnegative-integer? register? a86-value?))]
+          [emulator-memory-ref       (case-> (->                                address?  a86-value?)
+                                             (-> emulator?                      address?  a86-value?)
+                                             (-> emulator? nonnegative-integer? address?  a86-value?))]
+          [emulator-memory-ref/32    (case-> (->                                address?  a86-value/32-bit?)
+                                             (-> emulator?                      address?  a86-value/32-bit?)
+                                             (-> emulator? nonnegative-integer? address?  a86-value/32-bit?))]
+          [convert                   (-> integer? etype? result/c)]
+          [ptr-ref                   (->* [a86-value? etype?]
+                                          (nonnegative-integer?)
+                                          result/c)]
+          [step-count                (parameter/c integer?)]
+          [current-emulator          (parameter/c emulator?)]
+          [persist-current-emulator? (parameter/c boolean?)]
+          [run-emulator              (-> (listof instruction?) input-port? output-port?)]
+          [asm-emulate               (->* [(listof instruction?)]
+                                          [#:before   (-> any/c)
+                                           #:during   (-> any/c)
+                                           #:after    (-> any/c)
+                                           #:on-exit  (-> any/c any/c)
+                                           #:on-exn   (-> exn? any/c)
+                                           #:on-raise (-> any/c any/c)]
+                                          any/c)]
+          [asm-emulate/io            (->* [(listof instruction?)
+                                           input-port?]
+                                          [output-port?
+                                           #:before   (-> any/c)
+                                           #:during   (-> any/c)
+                                           #:after    (-> any/c)
+                                           #:on-exit  (-> any/c any/c)
+                                           #:on-exn   (-> exn? any/c)
+                                           #:on-raise (-> any/c any/c)]
+                                          any/c)]))
 
 (require "../ast.rkt"
          "../registers.rkt"
