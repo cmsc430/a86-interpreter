9f64bfcaab226c9a06ddbd81ba4c87dd574d22ad
Pierce Darragh <pierce.darragh@gmail.com>
2024-03-25 23:11:50 +0100

    Commit emergency stopgap measure to fix output capturing

--- a86/main.rkt
+++ a86/main.rkt
@@ -2,10 +2,25 @@
 
 (require "ast.rkt"
          "registers.rkt"
-         "interp.rkt"
+         "emulate.rkt"
+         "emulate/runtime.rkt"
          "printer.rkt")
+
+;; FIXME: This is all bad.
+(current-runtime fraud)
+
+(define current-objs void)
+
+(define (asm-interp instructions)
+  (asm-emulate instructions))
+
+(define (asm-interp/io instructions in)
+  (asm-emulate/io instructions (open-input-string in)))
 
 (provide (all-from-out "ast.rkt"
                        "registers.rkt"
-                       "interp.rkt"
-                       "printer.rkt"))
+                       "printer.rkt")
+         ;; FIXME
+         current-objs
+         asm-interp
+         asm-interp/io)
