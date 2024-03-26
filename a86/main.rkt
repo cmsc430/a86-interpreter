99af4215a2b042d91e458211baa772e335bf597b
Pierce Darragh <pierce.darragh@gmail.com>
2024-03-26 17:21:55 +0100

    Reorganize emergency stopgap measure

--- a86/main.rkt
+++ a86/main.rkt
@@ -4,23 +4,12 @@
          "registers.rkt"
          "emulate.rkt"
          "emulate/runtime.rkt"
+         "interp.rkt"
          "printer.rkt")
 
-;; FIXME: This is all bad.
-(current-runtime fraud)
-
-(define current-objs void)
-
-(define (asm-interp instructions)
-  (asm-emulate instructions))
-
-(define (asm-interp/io instructions in)
-  (asm-emulate/io instructions (open-input-string in)))
-
 (provide (all-from-out "ast.rkt"
+                       "emulate.rkt"
+                       "emulate/runtime.rkt"
+                       "interp.rkt"
                        "registers.rkt"
-                       "printer.rkt")
-         ;; FIXME
-         current-objs
-         asm-interp
-         asm-interp/io)
+                       "printer.rkt"))
