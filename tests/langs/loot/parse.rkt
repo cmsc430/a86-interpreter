e4dfa46421ecb087bfa75f8f0ad13dce88e36526
Pierce Darragh <pierce.darragh@gmail.com>
2024-07-25 17:08:30 -0600

    Begin work on Loot tests (currently failing IO)
    Not sure what's going wrong here, since this just copies how Iniquity does it
    and those tests run fine. I wonder if I did something to get Iniquity working
    that I forgot about...

---
+++ tests/langs/loot/parse.rkt
@@ -0,0 +1,98 @@
+#lang racket
+(provide parse parse-e parse-define)
+(require "ast.rkt")
+
+;; S-Expr ... -> Prog
+(define (parse . s)
+  (match s
+    [(cons (and (cons 'define _) d) s)
+     (match (apply parse s)
+       [(Prog ds e)
+        (Prog (cons (parse-define d) ds) e)])]
+    [(cons e '()) (Prog '() (parse-e e))]
+    [_ (error "program parse error")]))
+
+;; S-Expr -> Defn
+(define (parse-define s)
+  (match s
+    [(list 'define (list-rest (? symbol? f) xs) e)
+     (if (andmap symbol? xs)
+         (Defn f xs (parse-e e))
+         (error "parse definition error"))]
+    [_ (error "Parse defn error" s)]))
+
+;; S-Expr -> Expr
+(define (parse-e s)
+  (match s
+    [(? datum?)               (Lit s)]
+    ['eof                     (Eof)]
+    [(? symbol?)              (Var s)]
+    [(list 'quote (list))     (Lit '())]
+    [(list (? op0? p0))       (Prim0 p0)]
+    [(list (? op1? p1) e)     (Prim1 p1 (parse-e e))]
+    [(list (? op2? p2) e1 e2) (Prim2 p2 (parse-e e1) (parse-e e2))]
+    [(list (? op3? p3) e1 e2 e3)
+     (Prim3 p3 (parse-e e1) (parse-e e2) (parse-e e3))]
+    [(list 'begin e1 e2)
+     (Begin (parse-e e1) (parse-e e2))]
+    [(list 'if e1 e2 e3)
+     (If (parse-e e1) (parse-e e2) (parse-e e3))]
+    [(list 'let (list (list (? symbol? x) e1)) e2)
+     (Let x (parse-e e1) (parse-e e2))]
+    [(cons 'match (cons e ms))
+     (parse-match (parse-e e) ms)]
+    [(list (or 'lambda 'λ) (? (lambda (xs) (and (list? xs) (andmap symbol? xs)) xs) xs) e)
+     (Lam (gensym 'lambda) xs (parse-e e))]
+    [(cons e es)
+     (App (parse-e e) (map parse-e es))]
+    [_ (error "Parse error" s)]))
+
+;; Expr [Listof S-Expr]
+(define (parse-match e ms)
+  (match ms
+    ['() (Match e '() '())]
+    [(cons (list p r) ms)
+     (match (parse-match e ms)
+       [(Match e ps es)
+        (Match e
+               (cons (parse-pat p) ps)
+               (cons (parse-e r) es))])]
+    [_ (error "Parse match error" e ms)]))
+
+;; S-Expr -> Pat
+(define (parse-pat p)
+  (match p
+    [(? datum?) (Lit p)]
+    [(? symbol?) (Var p)]
+    [(list 'quote (list)) (Lit '())]
+    [(list 'box p)
+     (Box (parse-pat p))]
+    [(list 'cons p1 p2)
+     (Cons (parse-pat p1) (parse-pat p2))]
+    [(list 'and p1 p2)
+     (Conj (parse-pat p1) (parse-pat p2))]))
+
+
+;; Any -> Boolean
+(define (datum? x)
+  (or (exact-integer? x)
+      (boolean? x)
+      (char? x)
+      (string? x)))
+
+;; Any -> Boolean
+(define (op0? x)
+  (memq x '(read-byte peek-byte void)))
+
+(define (op1? x)
+  (memq x '(add1 sub1 zero? char? integer->char char->integer
+                 write-byte eof-object?
+                 box unbox empty? cons? box? car cdr
+                 vector? vector-length string? string-length)))
+
+(define (op2? x)
+  (memq x '(+ - < = eq? cons
+              make-vector vector-ref make-string string-ref)))
+
+(define (op3? x)
+  (memq x '(vector-set!)))
